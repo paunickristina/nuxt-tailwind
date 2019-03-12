a84dc35cf22704bbe5f307b65d31d64d64e2f211
Kristina Paunic <paunickristina@gmail.com>
2019-03-12 16:15:23 +0100

    tailwind test

---
+++ components/Product.vue
@@ -0,0 +1,36 @@
+<template>
+  <article class="bg-white mx-12 px-4 py-2 mb-6 lg:w-1/2 lg:mx-auto">
+		<div>
+			<h3 class="text-left text-3xl mb-4">
+				<span class="italic text-blue-light">1.</span>
+				Titre éditorialisé du produit
+			</h3>
+			<div class="flex flex-wrap">
+				<div class="w-1/2 text-right">
+					<div class="border-black border-solid border-2">
+						<img class="block" src="/converse.jpg" alt="">
+					</div>
+					<div class="border-black border-solid border-2 text-black font-bold py-2 px-4 my-2 inline-block">
+						<span>187,65&#8364;</span>
+					</div>
+					<p class="text-black font-bold py-2 px-4 my-2 inline-block line-through">224,12&#8364;</p>
+				</div>
+				<div class="w-1/2 pl-10 pr-2">
+					<h4 class="font-black text-xl mb-2">Nom de l’article qui peut être très long mais sur 2 lignes max</h4>
+					<p class="mb-8">Nom du marchand</p>
+					<button class="bg-blue text-white italic font-black uppercase text-xl px-8 py-2">Voir l’offre</button>
+				</div>
+			</div>
+		</div>
+	</article>
+</template>
+
+<script>
+	export default {
+
+	}
+</script>
+
+<style>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,60 +1,41 @@
 <template>
-  <section class="container">
+  <section class="wrapper">
     <div>
-      <logo />
-      <h1 class="title">
+      <h1 class="title text-center">
         nuxt-tailwind
       </h1>
-      <h2 class="subtitle">
-        My Nuxt.js project
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
-        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">
-          GitHub
-        </a>
-      </div>
+      <Product />
+      <Product />
     </div>
   </section>
 </template>
 
 <script>
-import Logo from "~/components/Logo.vue"
+import Product from "@/components/Product.vue"
 
 export default {
   components: {
-    Logo
+    Product
   }
 }
 </script>
 
-<style>
-.container {
+<style scoped lang="scss">
+.wrapper {
   min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
+  margin: 0 auto;
+  background: lightgray;
+
+  & .title {
+    font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
+      "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
+    display: block;
+    font-weight: 300;
+    font-size: 40px;
+    color: #35495e;
+    letter-spacing: 1px;
+    margin-bottom: 10px;
+  }
 }
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
-    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-.links {
-  padding-top: 15px;
-}
+
 </style>
